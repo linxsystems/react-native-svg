9044103c3dfdbb6062603c8a5df6d5aae0aeea87
Andrew Schenk <39064173+andrewschenk-linx@users.noreply.github.com>
2020-03-05 12:22:05 -0600

    Update SvgView.java

--- android/src/main/java/com/horcrux/svg/SvgView.java
+++ android/src/main/java/com/horcrux/svg/SvgView.java
@@ -21,7 +21,8 @@ import android.util.Base64;
 import android.view.View;
 import android.view.ViewParent;
 
-import androidx.annotation.NonNull;
+//import androidx.annotation.NonNull;
+import android.support.annotation.NonNull;
 
 import com.facebook.react.bridge.Dynamic;
 import com.facebook.react.bridge.ReactContext;
